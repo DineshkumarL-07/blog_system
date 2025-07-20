8d361227ad43433a58e3794a6b90b2069a54ac16
dinesh <ldineshkumar38@gmail.com>
2025-07-20 20:59:14 +0530

    Modified swagger UI setup in separate file.

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -17,28 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from rest_framework import permissions, routers
+from blog_system.swagger import schema_view
 
+BASE_URL = 'blog/'
 router = routers.DefaultRouter()
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Blog System API",
-      default_version='v1',
-      description="API documentation for Blog System",
-      terms_of_service="https://www.example.com/terms/",
-      contact=openapi.Contact(email="contact@example.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include(router.urls)),  # Your API endpoints
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(f'{BASE_URL}admin/', admin.site.urls),
+    path(f'{BASE_URL}', include(router.urls)),  # Your API endpoints
+    path(f'{BASE_URL}swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(f'{BASE_URL}redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
